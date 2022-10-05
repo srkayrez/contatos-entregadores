0e42e17bc5a92cc8f4844ac0ca9166d74dc02f92
Joao Caires <joao.caires@programmers.com.br>
2022-10-05 11:45:52 -0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
