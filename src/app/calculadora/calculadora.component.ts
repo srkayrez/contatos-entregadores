56855fcce4f105698fc72e306159e1039d7f9a9b
srkayrez <srkayrez@gmail.com>
2023-06-01 20:30:48 -0300

    add botoes de funcionalidades

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -10,6 +10,34 @@ export class CalculadoraComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const imageElement = document.getElementById('imagem');
+
+    imageElement.addEventListener('mouseover', () => {
+      imageElement.setAttribute('src', 'https://github.com/srkayrez/contatos-entregadores/blob/master/src/assets/route%20red.png?raw=true');
+    });
+    
+    imageElement.addEventListener('mouseout', () => {
+      imageElement.setAttribute('src', 'https://github.com/srkayrez/contatos-entregadores/blob/master/src/assets/route.png?raw=true');
+    });   
+
+    const imagemElement = document.getElementById('imagem2');
+
+    imagemElement.addEventListener('mouseover', () => {
+      imagemElement.setAttribute('src', 'https://github.com/srkayrez/contatos-entregadores/blob/master/src/assets/calculator%20red.png?raw=true');
+    });
+    
+    imagemElement.addEventListener('mouseout', () => {
+      imagemElement.setAttribute('src', 'https://github.com/srkayrez/contatos-entregadores/blob/master/src/assets/calculator.png?raw=true');
+    });   
+
+    const btnRotas = document.getElementById('btn-rotas')
+    const btnCalculadora = document.getElementById('btn-calculadora')
+
+    btnRotas.addEventListener('click',() => alert('Funcionalidade sendo criada'))
+    btnCalculadora.addEventListener('click',() => alert('Funcionalidade sendo criada'))
   }
+
+  
+
 
 }
